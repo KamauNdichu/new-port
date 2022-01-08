ec97e9d5c9e922cba1a2fbc8ee89d8afb34725d9
KJ Ndichu <kamaundichu754@gmail.com>
2022-01-08 11:15:27 +0300

    axios

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../../components/navbar/Navbar";
+import News from "../../components/news/News";
 import "./blog.css";
 
 export default function Blog() {
   return (
     <div className="blogContainer">
       <Navbar />
-      Blog
+      <News />
     </div>
   );
 }
