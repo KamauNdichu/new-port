6091bd3f2d60707f6a2107e560d795a9d4e35707
KamauNdichu <kamaundichu754@gmail.com>
2022-01-17 13:06:52 +0300

    add a project

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -11,6 +11,14 @@ const projects = [
     description:
       "This project is a deep learning model that aims to visually diagnose melanoma, the deadliest form of skin cancer. In particular, the algorithm will distinguish this malignant skin tumor from two types of benign lesions (nevi and seborrheic keratoses).The problem being addressed here apart from visually dignosing the malignant skin tumor is, trying to come up with a noninvasive, easy to use system that can be used by actual dermatologists in their work. It's also meant to reduce miss-classification of lesions and diagnosis time.",
   },
+  {
+    title: "Exercise tracker app",
+    img: "/img/excercise-tracker.png",
+    github: "https://github.com/KamauNdichu/exercise-tracker",
+    documentation: "#",
+    description:
+      "This project is a web application which will be later deployed cross platform to enable one to track their fitness journey. This project is aimed at mastering MERN stack",
+  },
 ];
 
 export default function Projects() {
