67c320df712264e67cc550d54481a3db86bbc8fe
KamauNdichu <kamaundichu754@gmail.com>
2022-01-17 12:17:14 +0300

    update projects

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -11,14 +11,6 @@ const projects = [
     description:
       "This project is a deep learning model that aims to visually diagnose melanoma, the deadliest form of skin cancer. In particular, the algorithm will distinguish this malignant skin tumor from two types of benign lesions (nevi and seborrheic keratoses).The problem being addressed here apart from visually dignosing the malignant skin tumor is, trying to come up with a noninvasive, easy to use system that can be used by actual dermatologists in their work. It's also meant to reduce miss-classification of lesions and diagnosis time.",
   },
-  {
-    title: "Netflix Clone using react",
-    img: "/img/1.jpg",
-    github: "https://github.com",
-    documentation: "/Kj_cv.pdf",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo vitae error consectetur exercitationem, aliquam porro ex alias mollitia? Architecto ex neque unde explicabo dolorem numquam non odit eum sapiente, cum alias optio doloremque dignissimos voluptatibus temporibus fugit amet voluptates accusantium ea voluptas vero. Neque necessitatibus itaque velit minus, architecto quod!",
-  },
 ];
 
 export default function Projects() {
